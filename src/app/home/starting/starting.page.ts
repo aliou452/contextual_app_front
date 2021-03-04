da3c8c886e7de8bf9b5bee4078890559c3e27734
unknown <dabdoulahi@gmail.com>
2021-03-04 11:33:30 +0000

    welcome page in completion

--- src/app/home/starting/starting.page.ts
+++ src/app/home/starting/starting.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-starting',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartingPage implements OnInit {
 
-  constructor() { }
+  constructor(public menuCtrl: MenuController) { }
 
   ngOnInit() {
+  }
+
+  openMenu(){
+    this.menuCtrl.open()
   }
 
 }
--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
@@ -12,8 +11,8 @@ export class WelcomePage implements OnInit {
     speed: 400
   };
   src1 = "https://www.orange.sn/sites/default/files/images/contact/ibou.svg"
-  constructor() { }
-
+  src2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSORaTJ-Tpq-QUIIiFP8JAdIHgai2S1TqtTbA&usqp=CAU"
+  src3 = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fclientespace.com%2Fwp-content%2Fuploads%2F2020%2F10%2Ftaris-orange-money-frais-retrait-argent.jpg&imgrefurl=https%3A%2F%2Fclientespace.com%2Forange-money-senegal-frais-et-tarif-de-transfert-et-retrait%2F&tbnid=G-Ed1jL0CN49HM&vet=12ahUKEwiF-ICAlZTvAhVM3RoKHdlnB7gQMygNegUIARDBAQ..i&docid=qdLP-n_7_yHn8M&w=193&h=261&q=orange%20money%20&safe=active&ved=2ahUKEwiF-ICAlZTvAhVM3RoKHdlnB7gQMygNegUIARDBAQ"
   ngOnInit() {
   }
 

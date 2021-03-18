da6968ec8ee0612e55f79bbe8e1a3df83d703e60
unknown <dabdoulahi@gmail.com>
2021-03-18 08:21:32 +0000

    situation initiale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-
   // {
   //   path: '',
   //   redirectTo: 'home',
